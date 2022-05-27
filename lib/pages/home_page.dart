99cd8bb670bb424f0e219638b8535af7fe61a719
Elvis Barrionuevo <ehbarrionuevo@gmail.com>
2022-05-26 20:09:35 -0500

    Init 4

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,3 @@
-
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -9,17 +8,17 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-
   List pokemonList = [];
 
   @override
-  initState(){
+  initState() {
     super.initState();
     getDataPokemon();
   }
 
-  getDataPokemon() async{
-    Uri _uri = Uri.parse("https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
+  getDataPokemon() async {
+    Uri _uri = Uri.parse(
+        "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
     http.Response response = await http.get(_uri);
     Map<String, dynamic> myMap = json.decode(response.body);
     pokemonList = myMap["pokemon"];
@@ -39,19 +38,68 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("PokedexApp"),
-      ),
-      body: ListView.builder(
-        itemCount: pokemonList.length,
-        itemBuilder: (BuildContext context, int index){
-          return ListTile(
-            leading: CircleAvatar(
-              backgroundImage: NetworkImage(pokemonList[index]["img"]),
+      // appBar: AppBar(
+      //   title: Text("PokedexApp"),
+      // ),
+      // body: ListView.builder(
+      //   itemCount: pokemonList.length,
+      //   itemBuilder: (BuildContext context, int index){
+      //     return ListTile(
+      //       leading: CircleAvatar(
+      //         backgroundImage: NetworkImage(pokemonList[index]["img"]),
+      //       ),
+      //       title: Text(pokemonList[index]["name"]),
+      //     );
+      //   },
+      // ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: Column(
+              children: [
+                Text(
+                  "Pokedex",
+                  style: TextStyle(
+                    fontSize: 26.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+
+                GridView.count(
+                  shrinkWrap: true,
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 12,
+                  crossAxisSpacing: 12,
+                  childAspectRatio: 1.3,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xff4ACFB0),
+                        borderRadius: BorderRadius.circular(18.0),
+                      ),
+
+                    ),
+
+
+
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xff4ACFB0),
+                      ),
+                    ),
+                    Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xff4ACFB0),
+                      ),
+                    ),
+                  ],
+                ),
+
+              ],
             ),
-            title: Text(pokemonList[index]["name"]),
-          );
-        },
+          ),
+        ),
       ),
     );
   }
