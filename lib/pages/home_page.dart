84b807fafca91a81b5431342d537ef812699143a
Chris-ASUS <humbertpt14@gmail.com>
2022-12-19 00:47:21 -0500

    lista pokemonList

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,101 +1,42 @@
 import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter_codigo5_pokedex/models/pokemon_model.dart';
-import 'package:flutter_codigo5_pokedex/ui/widgets/item_grid_widget.dart';
 import 'package:http/http.dart' as http;
 
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  List pokemonList = [];
-  List<PokemonModel> pokemonModelList = [];
-
-  @override
-  initState() {
-    super.initState();
-    getDataPokemon();
-  }
-
-  getDataPokemon() async {
+class HomePage extends StatelessWidget {
+  getData() async {
+    // https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json
     Uri _uri = Uri.parse(
         "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json");
     http.Response response = await http.get(_uri);
+    // print(response.statusCode); // statusCode 200 o 404
+    // print(response.body); //body es un getter que retorna un String
+    // print(json.decode(response.body));
     Map<String, dynamic> myMap = json.decode(response.body);
-    pokemonList = myMap["pokemon"];
-    pokemonModelList = pokemonList.map((e) => PokemonModel.fromJson(e)).toList();
-    setState(() {});
-    // myMap["pokemon"].forEach((mandarina){
-    //   print(mandarina['name']);
-    // });
+    // print(myMap["pokemon"]); //imprimiendo Lista "pokemon"
+    // print(myMap["pokemon"][0]["name"]); //imprimiendo a "Bulbasaur"
+    // print(myMap["pokemon"][0]["next_evolution"][0]["name"]); //imprimiendo a "Ivysaur"
 
-    // pokemonList.forEach((element) {
+    // myMap["pokemon"].forEach((element){
     //   print(element["name"]);
-    // });
-
+    // }); //extrayendo los "name" de los 150 pokemon [forma1]
+    //
     // for(var item in myMap["pokemon"]){
-    //   print(item["name"]);
+    //   print(item["name"]); //extrayendo los "name" de los 150 pokemon [forma2]
     // }
+
+    List pokemonList = myMap["pokemon"];
+    pokemonList.forEach((element) {
+      print(element["name"]);
+    });
   }
 
   @override
   Widget build(BuildContext context) {
+    getData();
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("PokedexApp"),
-      // ),
-      // body: ListView.builder(
-      //   itemCount: pokemonList.length,
-      //   itemBuilder: (BuildContext context, int index){
-      //     return ListTile(
-      //       leading: CircleAvatar(
-      //         backgroundImage: NetworkImage(pokemonList[index]["img"]),
-      //       ),
-      //       title: Text(pokemonList[index]["name"]),
-      //     );
-      //   },
-      // ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(12),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  "Pokedex",
-                  style: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
-                ),
-                const SizedBox(
-                  height: 30.0,
-                ),
-                GridView.count(
-                  physics: const ScrollPhysics(),
-                  shrinkWrap: true,
-                  crossAxisCount: 2,
-                  mainAxisSpacing: 12,
-                  crossAxisSpacing: 12,
-                  childAspectRatio: 1.35,
-                  children: pokemonModelList
-                      .map<Widget>(
-                        (pokemon) => ItemGridWidget(
-                          // name: pokemon.name,
-                          // image: pokemon.img,
-                          // type: pokemon.type,
-                          pokemon: pokemon,
-                        ),
-                      )
-                      .toList(),
-                ),
-              ],
-            ),
-          ),
-        ),
+      appBar: AppBar(
+        title: Text("Pokedex"),
       ),
     );
   }
