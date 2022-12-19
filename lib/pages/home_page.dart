fb3e58d82957b227781e0e5ee40c18ecdee01c81
Chris-ASUS <humbertpt14@gmail.com>
2022-12-19 00:54:22 -0500

    reordenando getData

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,7 +3,21 @@ import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List pokemonList = [];
+
+  @override
+  initState(){
+    super.initState();
+    getData();
+  }
+
   getData() async {
     // https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json
     Uri _uri = Uri.parse(
@@ -11,21 +25,8 @@ class HomePage extends StatelessWidget {
     http.Response response = await http.get(_uri);
     // print(response.statusCode); // statusCode 200 o 404
     // print(response.body); //body es un getter que retorna un String
-    // print(json.decode(response.body));
     Map<String, dynamic> myMap = json.decode(response.body);
-    // print(myMap["pokemon"]); //imprimiendo Lista "pokemon"
-    // print(myMap["pokemon"][0]["name"]); //imprimiendo a "Bulbasaur"
-    // print(myMap["pokemon"][0]["next_evolution"][0]["name"]); //imprimiendo a "Ivysaur"
-
-    // myMap["pokemon"].forEach((element){
-    //   print(element["name"]);
-    // }); //extrayendo los "name" de los 150 pokemon [forma1]
-    //
-    // for(var item in myMap["pokemon"]){
-    //   print(item["name"]); //extrayendo los "name" de los 150 pokemon [forma2]
-    // }
-
-    List pokemonList = myMap["pokemon"];
+    pokemonList = myMap["pokemon"];
     pokemonList.forEach((element) {
       print(element["name"]);
     });
@@ -33,7 +34,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    getData();
     return Scaffold(
       appBar: AppBar(
         title: Text("Pokedex"),
